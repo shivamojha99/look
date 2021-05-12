37ec5256561048d4999ec14f17d1b296f726c86b
satyendra tiwari <satyendra4030@gmail.com>
2021-05-12 22:23:14 +0530

    heroku deployment

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
+const path = require("path");
 
 const rooms = {};
 
@@ -36,5 +38,12 @@ io.on("connection", socket => {
 
 });
 
+if( process.env.PROD ) {
+    app.use( express.static(path.join(__dirname , './client/build')));
+    app.get('*', (req , res) =>{
+        res.sendFile(path.join(__dirname , './client/build/index.html'));
+    });
+}
 
-server.listen(8000, () => console.log('server is running on port 8000'));
+const port = process.env.PORT || 8000;
+server.listen(port, () => console.log(`server is running on port ${port}`));
